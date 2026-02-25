492680846064b84cd556ef4b687d66374daee531
Quang Khoa <khoatqhe150834@gmail.com>
2026-02-25 19:22:03 +0700

    feat: Implement user registration service along with UserRepository and RoleRepository.

---
+++ src/main/java/com/group3/accounttrade/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.group3.accounttrade.repository;
+
+import com.group3.accounttrade.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+}
