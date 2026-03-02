34f7dfa288a7f165801efb9b4cd2831157c5aa94
Quang Khoa <khoatqhe150834@gmail.com>
2026-03-02 22:29:06 +0700

    feat: Initialize Spring Boot application with security configuration, authentication flows, and dashboard templates.

--- src/main/java/com/group3/accounttrade/AccountTradeApplication.java
+++ src/main/java/com/group3/accounttrade/AccountTradeApplication.java
@@ -2,8 +2,10 @@ package com.group3.accounttrade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class AccountTradeApplication {
 
     public static void main(String[] args) {
