a01ac47d608803ba117f3b05cbe9de766df1cc82
Quang Khoa <khoatqhe150834@gmail.com>
2026-03-06 23:20:23 +0700

    feat: Implement initial data models, repositories, and services for cart, post, and category management.

---
+++ src/main/java/com/group3/accounttrade/entity/Cart.java
@@ -0,0 +1,33 @@
+package com.group3.accounttrade.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "Carts")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private Integer cartId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
